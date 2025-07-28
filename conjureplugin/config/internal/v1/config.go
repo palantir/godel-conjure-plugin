18f9b0626a616da2d34814ee6a3db2a2bfe67e33
Avi Radinsky <72529800+aviradinsky@users.noreply.github.com>
2025-07-28 17:07:23 -0400

    feature: add dedicated field for configuring assets of type `conjure-ir-extensions-provider` (#548)

--- conjureplugin/config/internal/v1/config.go
+++ conjureplugin/config/internal/v1/config.go
@@ -39,6 +39,8 @@ type SingleConjureConfig struct {
 	// AcceptFuncs indicates if we will generate lambda based visitor code.
 	// Currently this is behind a feature flag and is subject to change.
 	AcceptFuncs *bool `yaml:"accept-funcs,omitempty"`
+	// Extensions contain metadata for consumption by assets of type `conjure-ir-extensions-provider`.
+	Extensions map[string]any `yaml:"extensions,omitempty"`
 }
 
 type LocatorType string
