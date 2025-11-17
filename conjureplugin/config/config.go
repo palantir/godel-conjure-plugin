4202d586e7608a729af9912b36b7b016d14b4802
Avi Radinsky <72529800+aviradinsky@users.noreply.github.com>
2025-11-17 15:22:36 -0500

    feature: `v2` config (#643)

--- conjureplugin/config/config.go
+++ conjureplugin/config/config.go
@@ -15,23 +15,25 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"maps"
 	"net/url"
 	"os"
-	"path/filepath"
 	"slices"
 	"strings"
 
 	"github.com/palantir/godel-conjure-plugin/v6/conjureplugin"
-	v1 "github.com/palantir/godel-conjure-plugin/v6/conjureplugin/config/internal/v1"
-	"github.com/pkg/errors"
+	v2 "github.com/palantir/godel-conjure-plugin/v6/conjureplugin/config/internal/v2"
+	"github.com/palantir/godel-conjure-plugin/v6/conjureplugin/config/internal/validate"
+	pkgerror "github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
-type ConjurePluginConfig v1.ConjurePluginConfig
+type ConjurePluginConfig v2.ConjurePluginConfig
 
-func ToConjurePluginConfig(in *ConjurePluginConfig) *v1.ConjurePluginConfig {
-	return (*v1.ConjurePluginConfig)(in)
+func ToConjurePluginConfig(in *ConjurePluginConfig) *v2.ConjurePluginConfig {
+	return (*v2.ConjurePluginConfig)(in)
 }
 
 // ToParams returns the conjureplugin.ConjureProjectParams representation of the receiver. This function performs
@@ -44,21 +46,31 @@ func ToConjurePluginConfig(in *ConjurePluginConfig) *v1.ConjurePluginConfig {
 // Currently, if multiple Conjure projects have the same output directory (after normalization using filepath.Clean),
 // this is considered to be warning. The returned warning is an error created using errors.Join that contains one error
 // per output path shared by multiple projects.
-func (c *ConjurePluginConfig) ToParams() (_ conjureplugin.ConjureProjectParams, warnings []error, err error) {
+func (c *ConjurePluginConfig) ToParams() (_ conjureplugin.ConjureProjectParams, warnings []error, _ error) {
 	sortedKeys := slices.Sorted(maps.Keys(c.ProjectConfigs))
 
-	seenDirs := make(map[string][]string)
+	conflicts := ToConjurePluginConfig(c).OutputDirConflicts()
+
 	params := make(map[string]conjureplugin.ConjureProjectParam)
 	for _, key := range sortedKeys {
+		if err := validate.ValidateProjectName(key); err != nil {
+			return conjureplugin.ConjureProjectParams{}, nil, err
+		}
+
 		currConfig := c.ProjectConfigs[key]
 
-		// normalize outputDir
-		outputDir := filepath.Clean(currConfig.OutputDir)
-		seenDirs[outputDir] = append(seenDirs[outputDir], key)
+		outputDir := currConfig.ResolvedOutputDir(key)
+
+		if !currConfig.SkipDeleteGeneratedFiles && len(conflicts[key]) > 0 {
+			return conjureplugin.ConjureProjectParams{}, nil, errors.Join(append(
+				[]error{fmt.Errorf("project %q cannot delete generated files when output directories conflict", key)},
+				conflicts[key]...,
+			)...)
+		}
 
 		irProvider, err := (*IRLocatorConfig)(&currConfig.IRLocator).ToIRProvider()
 		if err != nil {
-			return conjureplugin.ConjureProjectParams{}, nil, errors.Wrapf(err, "failed to convert configuration for %s to provider", key)
+			return conjureplugin.ConjureProjectParams{}, nil, pkgerror.Wrapf(err, "failed to convert configuration for %s to provider", key)
 		}
 
 		groupID := c.GroupID
@@ -66,32 +78,41 @@ func (c *ConjurePluginConfig) ToParams() (_ conjureplugin.ConjureProjectParams, 
 			groupID = currConfig.GroupID
 		}
 
-		publishVal := false
+		var publishVal bool
 		// if value for "publish" is not specified, treat as "true" only if provider generates IR from YAML
 		if currConfig.Publish == nil {
 			publishVal = irProvider.GeneratedFromYAML()
+		} else {
+			publishVal = *currConfig.Publish
 		}
 		acceptFuncsFlag := true
 		if currConfig.AcceptFuncs != nil {
 			acceptFuncsFlag = *currConfig.AcceptFuncs
 		}
 		params[key] = conjureplugin.ConjureProjectParam{
-			OutputDir:   outputDir,
-			IRProvider:  irProvider,
-			AcceptFuncs: acceptFuncsFlag,
-			Server:      currConfig.Server,
-			CLI:         currConfig.CLI,
-			Publish:     publishVal,
-			GroupID:     groupID,
+			OutputDir:                outputDir,
+			IRProvider:               irProvider,
+			AcceptFuncs:              acceptFuncsFlag,
+			Server:                   currConfig.Server,
+			CLI:                      currConfig.CLI,
+			Publish:                  publishVal,
+			GroupID:                  groupID,
+			SkipDeleteGeneratedFiles: currConfig.SkipDeleteGeneratedFiles,
 		}
 	}
 
-	for _, outputDir := range slices.Sorted(maps.Keys(seenDirs)) {
-		projects := seenDirs[outputDir]
-		if len(projects) <= 1 {
-			continue
+	var err error
+	if !c.AllowConflictingOutputDirs {
+		for _, key := range sortedKeys {
+			err = errors.Join(append([]error{err}, conflicts[key]...)...)
 		}
-		warnings = append(warnings, errors.Errorf("Projects %v are configured with the same outputDir %q, which may cause conflicts when generating Conjure output", projects, outputDir))
+		if err != nil {
+			return conjureplugin.ConjureProjectParams{}, nil, fmt.Errorf("output directory conflicts detected: %w", err)
+		}
+	}
+
+	for _, key := range sortedKeys {
+		warnings = append(warnings, conflicts[key]...)
 	}
 
 	return conjureplugin.ConjureProjectParams{
@@ -100,73 +121,77 @@ func (c *ConjurePluginConfig) ToParams() (_ conjureplugin.ConjureProjectParams, 
 	}, warnings, nil
 }
 
-type SingleConjureConfig v1.SingleConjureConfig
+type SingleConjureConfig v2.SingleConjureConfig
 
-func ToSingleConjureConfig(in *SingleConjureConfig) *v1.SingleConjureConfig {
-	return (*v1.SingleConjureConfig)(in)
+func ToSingleConjureConfig(in *SingleConjureConfig) *v2.SingleConjureConfig {
+	return (*v2.SingleConjureConfig)(in)
 }
 
-type LocatorType v1.LocatorType
+type LocatorType v2.LocatorType
 
-type IRLocatorConfig v1.IRLocatorConfig
+type IRLocatorConfig v2.IRLocatorConfig
 
-func ToIRLocatorConfig(in *IRLocatorConfig) *v1.IRLocatorConfig {
-	return (*v1.IRLocatorConfig)(in)
+func ToIRLocatorConfig(in *IRLocatorConfig) *v2.IRLocatorConfig {
+	return (*v2.IRLocatorConfig)(in)
 }
 
 func (cfg *IRLocatorConfig) ToIRProvider() (conjureplugin.IRProvider, error) {
 	if cfg.Locator == "" {
-		return nil, errors.Errorf("locator cannot be empty")
+		return nil, pkgerror.Errorf("locator cannot be empty")
 	}
 
 	locatorType := cfg.Type
-	if locatorType == "" || locatorType == v1.LocatorTypeAuto {
+	if locatorType == "" || locatorType == v2.LocatorTypeAuto {
 		if parsedURL, err := url.Parse(cfg.Locator); err == nil && parsedURL.Scheme != "" {
 			// if locator can be parsed as a URL and it has a scheme explicitly specified, assume it is remote
-			locatorType = v1.LocatorTypeRemote
+			locatorType = v2.LocatorTypeRemote
 		} else {
 			// treat as local: determine if path should be used as file or directory
 			switch lowercaseLocator := strings.ToLower(cfg.Locator); {
 			case strings.HasSuffix(lowercaseLocator, ".yml") || strings.HasSuffix(lowercaseLocator, ".yaml"):
-				locatorType = v1.LocatorTypeYAML
+				locatorType = v2.LocatorTypeYAML
 			case strings.HasSuffix(lowercaseLocator, ".json"):
-				locatorType = v1.LocatorTypeIRFile
+				locatorType = v2.LocatorTypeIRFile
 			default:
 				// assume path is to local YAML directory
-				locatorType = v1.LocatorTypeYAML
+				locatorType = v2.LocatorTypeYAML
 
 				// if path exists and is a file, treat path as an IR file
 				if fi, err := os.Stat(cfg.Locator); err == nil && !fi.IsDir() {
-					locatorType = v1.LocatorTypeIRFile
+					locatorType = v2.LocatorTypeIRFile
 				}
 			}
 		}
 	}
 
 	switch locatorType {
-	case v1.LocatorTypeRemote:
+	case v2.LocatorTypeRemote:
 		return conjureplugin.NewHTTPIRProvider(cfg.Locator), nil
-	case v1.LocatorTypeYAML:
+	case v2.LocatorTypeYAML:
 		return conjureplugin.NewLocalYAMLIRProvider(cfg.Locator), nil
-	case v1.LocatorTypeIRFile:
+	case v2.LocatorTypeIRFile:
 		return conjureplugin.NewLocalFileIRProvider(cfg.Locator), nil
 	default:
-		return nil, errors.Errorf("unknown locator type: %s", locatorType)
+		return nil, pkgerror.Errorf("unknown locator type: %s", locatorType)
 	}
 }
 
 func ReadConfigFromFile(f string) (ConjurePluginConfig, error) {
 	bytes, err := os.ReadFile(f)
 	if err != nil {
-		return ConjurePluginConfig{}, errors.WithStack(err)
+		return ConjurePluginConfig{}, pkgerror.WithStack(err)
 	}
 	return ReadConfigFromBytes(bytes)
 }
 
 func ReadConfigFromBytes(inputBytes []byte) (ConjurePluginConfig, error) {
-	var cfg ConjurePluginConfig
-	if err := yaml.UnmarshalStrict(inputBytes, &cfg); err != nil {
-		return ConjurePluginConfig{}, errors.WithStack(err)
+	configBytes, err := UpgradeConfig(inputBytes)
+	if err != nil {
+		return ConjurePluginConfig{}, pkgerror.Wrapf(err, "failed to upgrade configuration")
 	}
-	return cfg, nil
+	var cfg v2.ConjurePluginConfig
+	if err := yaml.UnmarshalStrict(configBytes, &cfg); err != nil {
+		return ConjurePluginConfig{}, pkgerror.WithStack(err)
+	}
+	return ConjurePluginConfig(cfg), nil
 }
