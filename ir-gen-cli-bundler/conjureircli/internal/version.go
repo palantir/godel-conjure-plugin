fb1dd078a96712930db47e800bb54a7ab42bc9fd
Tony Abboud <tabboud@palantir.com>
2026-02-06 20:09:33 -0800

    Update conjure compiler to 4.54.0

--- ir-gen-cli-bundler/conjureircli/internal/version.go
+++ ir-gen-cli-bundler/conjureircli/internal/version.go
@@ -1,6 +1,6 @@
 package internal
 
 const (
-	Version = "4.35.0"
-	SHA256  = "5087877495d936504fccdb7f8746c51f5394b2667b3839d98c5fbd59c7add93f"
+	Version = "4.54.0"
+	SHA256  = "c932067863f2e4e13b190952d8910ec0d6192be5fc610318e24746219e9ef219"
 )
